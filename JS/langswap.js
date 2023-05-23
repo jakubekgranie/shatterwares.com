ba590d71728b421d6d24e5d4d11bf170c5a3bd0f
jakubekgranie <kawkagaming@interia.pl>
2023-05-23 21:26:05 +0200

    Registered Update 24: "CSS Reformation"
    - Started reformatting the css for the main site to make it reusable and clear;
    - Fixed the Discord button's contents overflowing

--- JS/langswap.js
+++ JS/langswap.js
@@ -1,34 +1,36 @@
 function changeLanguage() 
 {
+    var langbutton = document.getElementById('langswap').classList;
+    var htmllang = document.documentElement;
+    
     if(document.documentElement.getAttribute("lang") == "pl")
     {
-        document.getElementById('langswap').classList.add("england");
-        document.getElementById('langswap').classList.remove("poland");
-        document.documentElement.setAttribute('lang', "en");
+        langbutton.add("england");
+        langbutton.remove("poland");
+        htmllang.setAttribute('lang', "en");
     }
     else
     {
-        document.getElementById('langswap').classList.add("poland");
-        document.getElementById('langswap').classList.remove("england");
-        document.documentElement.setAttribute('lang', "pl");
+        langbutton.add("poland");
+        langbutton.remove("england");
+        htmllang.setAttribute('lang', "pl");
     }
 }
 function discordAnim()
 {
-    if(document.getElementById('dcicon').classList.contains('discordicontransformed') == false)
+    var footerimage = document.getElementById('footerimagediscord').classList;
+    var dcicon = document.getElementById('dcicon').classList;
+
+    if(dcicon.contains('discordicontransformed') == false)
     {
-        document.getElementById('footerimagediscord').classList.add('invert');
-        document.getElementById('dcolor').classList.remove('colora');
-        document.getElementById('dcolor').classList.add('colorb');
-        document.getElementById('dcicon').classList.remove('discordicon');
-        document.getElementById('dcicon').classList.add('discordicontransformed');
+        footerimage.toggle('invert');
+        dcicon.toggle('discordicon');
+        dcicon.toggle('discordicontransformed');
     }
     else
     {
-        document.getElementById('footerimagediscord').classList.remove('invert');
-        document.getElementById('dcolor').classList.remove('colorb');
-        document.getElementById('dcolor').classList.add('colora');
-        document.getElementById('dcicon').classList.add('discordicon');
-        document.getElementById('dcicon').classList.remove('discordicontransformed');
+        footerimage.toggle('invert');
+        dcicon.toggle('discordicon');
+        dcicon.toggle('discordicontransformed');
     }
 }
