05be47c015318a0754e6b4530ec144fcff75398f
jakubekgranie <kawkagaming@interia.pl>
2023-05-26 14:58:34 +0200

    Registered Update 25 - "The CSS Finalization Update"
    // ShWHP
    - Finished reformatting CSS;
    - Added proper hover pseudo-class to the Discord button;
    - Changed the profile picture.
    
    //ShWPH
    - Made all paragraphs bolder;
    - Changed the media query responsible for mobile screen adaptation to activate on certain aspect ratios.

--- JS/langswap.js
+++ JS/langswap.js
@@ -18,18 +18,22 @@ function changeLanguage()
 }
 function discordAnim()
 {
-    var footerimage = document.getElementById('footerimagediscord').classList;
+    var footerimage = document.getElementById('footerimage_discord').classList;
     var dcicon = document.getElementById('dcicon').classList;
 
     if(dcicon.contains('discordicontransformed') == false)
     {
         footerimage.toggle('invert');
+        footerimage.toggle('footerimage_discord');
+        footerimage.toggle('footerimage_discord_inv');
         dcicon.toggle('discordicon');
         dcicon.toggle('discordicontransformed');
     }
     else
     {
         footerimage.toggle('invert');
+        footerimage.toggle('footerimage_discord');
+        footerimage.toggle('footerimage_discord_inv');
         dcicon.toggle('discordicon');
         dcicon.toggle('discordicontransformed');
     }
