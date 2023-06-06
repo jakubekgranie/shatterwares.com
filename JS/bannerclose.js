14d46c74f4fc25c8ebf91408be9e827ae45c1468
jakubekgranie <kawkagaming@interia.pl>
2023-06-06 21:44:58 +0200

    Registered Update 35 - "ReStart"
    // ShWHP
    - Purged the site in order to remodel the project. See "Releases".

--- JS/bannerclose.js
+++ JS/bannerclose.js
@@ -1,7 +1,3 @@
 function bannerClose(bannerid){
     document.getElementById(bannerid).style.display='none';
-    if (bannerid == "bner1")
-    {
-        document.getElementById('bner2').style.top = "5vw";
-    }
 }
