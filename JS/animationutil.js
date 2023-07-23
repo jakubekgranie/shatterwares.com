9fde944121e51ef340030752caa54433b60ae4c6
jakubekgranie <kawkagaming@interia.pl>
2023-07-23 19:39:00 +0200

    RegUpd 46
    //ShWHP
    - Minor HTML changes;
    - Truncated some JS by implementing potential (but unnoticed) loops;
    - Added a new pfp, which is, most probably, here to stay.

--- JS/animationutil.js
+++ JS/animationutil.js
@@ -2,12 +2,12 @@ var check = 0;
 var lastButton;
 
 function startAnimation1(){
-    document.getElementById("vid_bg").play();
-
     document.getElementById('loading_header').classList.add('dp-none');
-    animationForwarder('animelem1-1', 'animelem1-2', 'anim-1', 0)
-    document.getElementById('animelem1-1').classList.remove('dp-none');
-    document.getElementById('animelem1-2').classList.remove('dp-none');
+    const anim_elements = ['animelem1-1', 'animelem1-2'];
+    animationForwarder(anim_elements[0], anim_elements[1], 'anim-1', 0)
+    for(let i = 0; i < 2; i++){
+        document.getElementById(anim_elements[i]).classList.remove('dp-none');
+    }
     const animelem13 = document.getElementById('animelem1-3');
     animelem13.classList.add('anim-1');
     animelem13.classList.remove('dp-none');
@@ -16,7 +16,7 @@ function startAnimation1(){
 function anFrwdPreset(){
     if(check == 0){
         check = 1;
-        anFrwdDoubleExec('sectionlang1', 'sectionlang2', 'slash1-lang', 'slash2-lang', 'anim-fadeintb', 250, 'anim-blink', 100, 0);
+        anFrwdDoubleExec('sectionlang1', 'sectionlang2', 'slash1-lang', 'slash2-lang', 'anim-fadeintb', 250, 'anim-blink', 100);
         animationForwarder('skip', 'langCentral', 'anim-fadeinmod', 650)
     }
 }
@@ -25,11 +25,8 @@ function langPicker(buttonid){
         const langDiv = document.getElementById('sectionlang1');
         const langParagraph = document.getElementById('sectionlang2');
         const langHeading = document.getElementById('sectionlang1p2');
-        
-        const cpp = document.getElementById('cpp');
-        const html = document.getElementById('html');
-        const js = document.getElementById('js');
-        const css = document.getElementById('css');
+        const langNames = ['cpp','html','js','css'];
+        const languageId = [document.getElementById('cpp'), document.getElementById('html'), document.getElementById('js'), document.getElementById('css')];
 
         if(lastButton != null){
             const lastImage = 'icon-' + lastButton;
@@ -42,21 +39,14 @@ function langPicker(buttonid){
     
         const n_a = "not-allowed";
 
-        cpp.onclick = "null";
-        html.onclick = "null";
-        js.onclick = "null";
-        css.onclick = "null";
+        for(let i = 0; i < 4; i++){
+            languageId[i].onclick = "null";
+            languageId[i].style.cursor = n_a;
+        }
 
-        cpp.style.cursor = n_a;
-        html.style.cursor = n_a;
-        js.style.cursor = n_a;
-        css.style.cursor = n_a;
-
-        setTimeout(() => {cpp.style.cursor = null;
-            html.style.cursor = null;
-            js.style.cursor = null;
-            css.style.cursor = null;
-
+        setTimeout(() => {for(let i = 0; i < 4; i++){
+                languageId[i].style.cursor = null;
+            }
             document.getElementById(buttonid).style.cursor = n_a;
         }, 1000);
 
@@ -72,34 +62,26 @@ function langPicker(buttonid){
         setTimeout(() => {langParagraph.classList.add('animSwitchByFadeInLang')}, 100);
         setTimeout(() => {
     
-            setTimeout(() => {document.getElementById('sectionlang1').classList.remove('animSwitchByFadeInLang');
+            setTimeout(() => {
+                langDiv.classList.remove('animSwitchByFadeInLang');
                 langParagraph.classList.remove('animSwitchByFadeInLang');
-                document.getElementById('cpp').onclick = function() {langPicker('cpp')};
-                document.getElementById('html').onclick = function() {langPicker('html')};
-                document.getElementById('js').onclick = function() {langPicker('js')};
-                document.getElementById('css').onclick = function() {langPicker('css')};}, 490);
+                for(let i = 0; i < 4; i++){
+                    document.getElementById(langNames[i]).onclick = function() {langPicker(langNames[i])};
+                }}, 490);
 
-            if(buttonid == 'cpp'){
-                langHeading.textContent = "C++";
-                langParagraph.textContent = "C++ is one of the most renowned programming languages in the world, used in making a wide variety of programs/apps, mostly direct executables. I want to be fully competent at it, even though it is not one of the languages I'll need as a webmaster.";
-            }
-            if(buttonid == 'html'){
-                langHeading.textContent = "HTML";
-                langParagraph.textContent = "HTML's my personal favorite for obvious reasons: its potential for shaping something creative when associated, nesting style and overall not limiting my imagination. I just love doing stuff in it.";
-            }
-            if(buttonid == 'js'){
-                langHeading.textContent = "Javascript";
-                langParagraph.textContent = "Javascript is one of my recent 'discoveries'. Immediately after I dived into it, determination sprung into action. Being very powerful, my persona constantly tries to learn something new about it to empower my web developer adventures with fancy techniques.";
-            }
-            if(buttonid == 'css'){
-                langHeading.textContent = "CSS";
-                langParagraph.textContent = "There's not much to say here except for that it's a critical extension of html, or at least I perceive it as. Being a key to expressing one's power, I admire the fact that I'll carry it between the palms of my hand till the very end.";
+            const titles = ['C++','HTML','Javascript','CSS'];
+            const descriptions = ["C++ is one of the most renowned programming languages in the world, used in making a wide variety of programs/apps, mostly direct executables. I want to be fully competent at it, even though it is not one of the languages I'll need as a webmaster.","HTML's my personal favorite for obvious reasons: its potential for shaping something creative when associated, nesting style and overall not limiting my imagination. I just love doing stuff in it.","Javascript is one of my recent 'discoveries'. Immediately after I dived into it, determination sprung into action. Being very powerful, my persona constantly tries to learn something new about it to empower my web developer adventures with fancy techniques.","There's not much to say here except for that it's a critical extension of html, or at least I perceive it as. Being a key to expressing one's power, I admire the fact that I'll carry it between the palms of my hand till the very end."]
+            for(let i = 0; i < 4; i++){
+                if(buttonid == langNames[i]){
+                    langHeading.textContent = titles[i];
+                    langParagraph.textContent = descriptions[i];
+                }
             }
         }, 510);
     }
 }
 function langStats(){
-    anFrwdDoubleExec('sectionlang12', 'sectionlang22', 'slash1-lang2', 'slash2-lang2', 'anim-fadeintb', 250, 'anim-blink', 100, 0);
+    anFrwdDoubleExec('sectionlang12', 'sectionlang22', 'slash1-lang2', 'slash2-lang2', 'anim-fadeintb', 250, 'anim-blink', 100);
 
     const ids = ['html', 'css', 'cpp', 'js'];
     setTimeout(() => {
@@ -124,9 +106,9 @@ function unravelBars(langId){
     }
 }
 
-function anFrwdDoubleExec(identifier, identifier2, identifier3, identifier4 ,anim_id, wait_time, anim_id2, wait_time2, secondExecutionWaitTime){
+function anFrwdDoubleExec(identifier, identifier2, identifier3, identifier4 ,anim_id, wait_time, anim_id2, wait_time2){
     animationForwarder(identifier, identifier2, anim_id, wait_time);
-    setTimeout(() => {animationForwarder(identifier3, identifier4, anim_id2, wait_time2)}, secondExecutionWaitTime);
+    animationForwarder(identifier3, identifier4, anim_id2, wait_time2);
 }
 function animationForwarder(identifier, identifier2, anim_id, wait_time){
     if(identifier != 'skip'){
